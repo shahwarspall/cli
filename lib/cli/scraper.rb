0817c5268799e20667896dc1182179b8cfce0a02
shahwarspall <shevy.spall@gmail.com>
2019-09-12 01:22:04 -0400

    first

--- lib/cli/scraper.rb
+++ lib/cli/scraper.rb
@@ -1,4 +1,8 @@
-practice
+class TopCompanies::scraper
 
-
-sssss
+    def self.scrape 
+        site = "https://www.monster.com/company/?intcid=skr_navigation_www_company"
+        doc = page.css("div.company_logo")
+        page = Nokogiri::HTML(open(site))
+    end 
+end 
