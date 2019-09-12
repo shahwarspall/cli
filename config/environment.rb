2405d54cc5f8ac649b2ecf5cd91ef48b996e1fc3
shahwarspall <shevy.spall@gmail.com>
2019-09-12 01:21:09 -0400

    first

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/cli/scraper'
+require_relative '../lib/cli/restaurant'
+require_relative '../lib/cli/cli'
+require_relative '../lib/cli/version'
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,16 @@
-require "cli/version"
+require_relative "./Cli/version"
+require_relative "./Cli/company"
+require_relative "./Cli/scraper"
+require_relative "./Cli/cli"
+
+require "colorize"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 
 module Cli
-  class Error < StandardError; end
-  # Your code goes here...
+    class Error < StandardError; end
 end
+
+require_relative '../config/environment'
