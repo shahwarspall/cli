5b541f2c58815b3b87cee19a44cc4bdde603b3b5
shahwarspall <shevy.spall@gmail.com>
2019-09-13 01:21:51 -0400

    2nd command

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -1,3 +1,10 @@
 class Topcompanies:Cli
 
+    def call 
+        Topcompanies::Scraper.new.hiring_companies
+        puts "Welcome to the Top Companies hiring now!"
+        Begin
 end
+
+def Begin
+    
--- lib/cli/scraper.rb
+++ lib/cli/scraper.rb
@@ -2,7 +2,7 @@ class TopCompanies::Scraper
 
     def self.scrape 
         site = "https://www.monster.com/company/?intcid=skr_navigation_www_company"
-        doc = page.css("div.company_logo")
+        page = page.css("div.company_logo")
         page = Nokogiri::HTML(open(site))
     end 
 end 
